efab1b5721d4211eff27d6722f9ed082e350c22f
Daksh 🐧 <daksh_249062@saitm.ac.in>
2025-11-12 16:26:00 +0530

    Fix import case for Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Login from './Login';
+import Login from './login';
 import About from './About';
 import Contact from './Contact';
 import './App.css';
